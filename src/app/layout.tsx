3a9e68fcb202c620fcc256cf48f4c7b29caf901d
arfadmuzali <arfadmuzali258@gmail.com>
2024-05-05 17:43:00 +0700

    fix: metadata information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,16 @@ export const metadata: Metadata = {
     description: "Beli produk mebel di Eli Mebel",
     url: "https://eli-mebel.vercel.app/",
     siteName: "Eli Mebel",
-    images: [
-      {
-        url: "https://eli-mebel.vercel.app/img/pak-eli-screenshot.png",
-      },
-    ],
+    images: "https://eli-mebel.vercel.app/img/pak-eli-screenshot.png",
+    type: "website",
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "Eli Mebel",
+    description: "Beli produk mebel di Eli Mebel",
+    images: ["https://eli-mebel.vercel.app/img/pak-eli-screenshot.png"],
+  },
+  creator: "Arfad Muzali",
 };
 
 export default function RootLayout({
