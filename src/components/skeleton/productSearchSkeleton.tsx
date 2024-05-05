68f6b9bd2879f3a898f2771b3e7a7089c0feedb8
arfadmuzali <arfadmuzali258@gmail.com>
2024-05-05 10:58:22 +0700

    fix: key missing

--- src/components/skeleton/productSearchSkeleton.tsx
+++ src/components/skeleton/productSearchSkeleton.tsx
@@ -4,9 +4,9 @@ export default function ProductSearchSkeleton() {
   const temp: string[] = new Array(12).fill("");
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-10 gap-16 w-max justify-center items-center m-auto">
-      {temp.map((e) => {
+      {temp.map((e, index) => {
         return (
-          <div>
+          <div key={index}>
             <Skeleton className="lg:w-72 lg:h-72 w-36 h-36 mb-2" />
             <Skeleton className="w-36 h-5" />
           </div>
