72564534750e597ba214f1ddf00dc4771e902f98
dssauser24 <arfadmuzali258@gmail.com>
2024-10-19 10:29:56 +0700

    fix: footer layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -17,7 +17,7 @@ export default async function Footer() {
   const response = await getCategoryProduct();
   return (
     <div className="w-full justify-center items-start p-5 text-sm border-t border-stone-300 bg-stone-700 text-white">
-      <div className="flex md:flex-row flex-col justify-start gap-20 items-start mb-5 my-2 mx-3 ">
+      <div className="flex md:flex-row flex-col justify-start lg:justify-between gap-20 items-start mb-5 my-2 lg:mx-10 mx-4 ">
         <div className="m-auto md:m-0">
           <Image
             src={"/icon/icon-white.svg"}
@@ -27,51 +27,53 @@ export default async function Footer() {
           />
         </div>
 
-        <div className="flex gap-4 flex-col">
-          <h1 className="md:text-xl text-2xl font-bold">Halaman</h1>
-          <Link
-            href={"/"}
-            className="hover:text-stone-500 text-base md:text-sm "
-          >
-            Home
-          </Link>
-          <Link
-            href={"/product"}
-            className="hover:text-stone-500 text-base md:text-sm "
-          >
-            Produk
-          </Link>
-          <Link
-            href={"/about"}
-            className="hover:text-stone-500 text-base md:text-sm "
-          >
-            Tentang Kami
-          </Link>
-          <Link
-            href={"/contact"}
-            className="hover:text-stone-500 text-base md:text-sm "
-          >
-            Contact
-          </Link>
-        </div>
+        <div className="flex flex-col lg:flex-row lg:gap-24 gap-10 ">
+          <div className="flex gap-4 flex-col">
+            <h1 className="md:text-xl text-2xl font-bold">Halaman</h1>
+            <Link
+              href={"/"}
+              className="hover:text-stone-500 text-base md:text-sm "
+            >
+              Home
+            </Link>
+            <Link
+              href={"/product"}
+              className="hover:text-stone-500 text-base md:text-sm "
+            >
+              Produk
+            </Link>
+            <Link
+              href={"/about"}
+              className="hover:text-stone-500 text-base md:text-sm "
+            >
+              Tentang Kami
+            </Link>
+            <Link
+              href={"/contact"}
+              className="hover:text-stone-500 text-base md:text-sm "
+            >
+              Contact
+            </Link>
+          </div>
 
-        <div className="flex gap-4 flex-col">
-          <h1 className="md:text-xl text-2xl font-bold">Category</h1>
-          {response?.map((item) => {
-            return (
-              <Link
-                key={item.id}
-                href={"/"}
-                className="hover:text-stone-500 text-base md:text-sm"
-              >
-                {item.name}
-              </Link>
-            );
-          })}
+          <div className="flex gap-4 flex-col">
+            <h1 className="md:text-xl text-2xl font-bold">Category</h1>
+            {response?.map((item) => {
+              return (
+                <Link
+                  key={item.id}
+                  href={"/"}
+                  className="hover:text-stone-500 text-base md:text-sm"
+                >
+                  {item.name}
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </div>
 
-      <div className="border-t w-full text-center pt-2">
+      <div className="border-t w-full text-center pt-4">
         <h1>
           Build with love by{" "}
           <Link href={"https://github.com/arfadmuzali"} className="">
